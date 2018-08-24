c485b1c6e7c3e5827b27a9753e09f442820fff67
jcastro15 <castrojake99@gmail.com>
2018-08-23 23:22:27 -0400

    Still error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 var exphbs = require('express-handlebars');
 
 var app = express();
